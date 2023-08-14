1d38ca1334bc254aaef5a964745af6942e9583ef
TetsuyaAdachi <tetsuya_adachi@demia.co.jp>
2023-08-14 16:52:52 +0900

    jwt に変更

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from api.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ["id", "username", "email", "is_staff"]
+    list_filter = []
+    ordering = []
+    filter_horizontal = []
+    fieldsets = (
+        (
+            None,
+            {"fields": ("email",)},
+        ),
+        (
+            ("Permissions"),
+            {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")},
+        ),
+    )
--- api/models.py
+++ api/models.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    username = models.CharField("ユーザー名", max_length=30, blank=True, null=True)
+    email = models.EmailField("メールアドレス", unique=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
